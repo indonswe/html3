4a0f356ec8a763c1f969c3e6f1f02a25ac93fff9
indonswe <afc2002afc@hotmail.com>
2021-03-22 13:37:22 +0100

    Initial commit

---
+++ src/main/java/com/example/html1/controllers/Home.java
@@ -0,0 +1,16 @@
+package com.example.html1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+    @GetMapping("/index")
+    public String index(){
+        return null;
+    }
+    @GetMapping("/contact ")
+    public String contact(){
+        return null;
+    }
+}
