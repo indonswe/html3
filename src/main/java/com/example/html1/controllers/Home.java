148d189f08fb8d385cdf9064a73031ecdef61097
indonswe <afc2002afc@hotmail.com>
2021-03-22 18:43:59 +0100

    exercise-2 version 3

--- src/main/java/com/example/html1/controllers/Home.java
+++ src/main/java/com/example/html1/controllers/Home.java
@@ -1,12 +1,17 @@
 package com.example.html1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class Home {
+    List<String> inputString = new ArrayList<>();
     @GetMapping("/index")
     public String index(){
         return null;
@@ -17,6 +22,14 @@ public class Home {
     }
     @PostMapping("/contact ")
     public String contact(@RequestParam String string){
+        inputString.add(string);
         return string;
     }
+    @GetMapping("/contactlist")
+    public String contactList(Model model) {
+        model.addAttribute(inputString);
+        return "contactlist";
+    }
+
+
 }
